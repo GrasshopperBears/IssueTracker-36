6c0bd33350ec166018eb6d604b6ac7510c991234
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-18 01:47:28 +0900

    안내문구 수정

--- frontend/pages/NotFoundPage.jsx
+++ frontend/pages/NotFoundPage.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 const NotFoundPage = () => (
   <AuthPageLayout>
     <Message>Page Not Found :(</Message>
+    <Message>처음 진입하셨을 경우 새로고침을 눌러주세요</Message>
   </AuthPageLayout>
 );
 
